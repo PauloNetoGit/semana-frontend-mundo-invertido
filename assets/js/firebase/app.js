2b6b0403eb78f3a2082eb73fb1da379e30e9220d
Paulo Neto <30964999+PauloNetoGit@users.noreply.github.com>
2022-09-14 09:59:45 -0300

    Add pastas js e musics

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCq_r_-SXRWL6qrLKHTHjYdznKUCB6-9I",
+  authDomain: "mundo-invertido-ca45d.firebaseapp.com",
+  projectId: "mundo-invertido-ca45d",
+  storageBucket: "mundo-invertido-ca45d.appspot.com",
+  messagingSenderId: "751052998658",
+  appId: "1:751052998658:web:79752022b6aaa6db013f1b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
